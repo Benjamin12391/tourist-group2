68b6a30e64379a306c54f07cc59b58a734697836
Benjamin12391 <ben.gruber1@yahoo.com>
2026-03-24 14:43:02 +0100

    2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+
+//id,rooms,usedrooms,beds,usedbeds,year,month
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        String path="src/main/resources/Hotels.csv";
+        Scanner sc = new Scanner(new File(path));
+        List<Hotel> hotels=new ArrayList<>();
+        while (sc.hasNextLine()) {
+            String l=sc.nextLine();
+            String[] p=l.split(",");
+
+                int id =Integer.parseInt(p[0]);
+                int room =Integer.parseInt(p[1]);
+                int usedrooms =Integer.parseInt(p[2]);
+                int beds =Integer.parseInt(p[3]);
+                int usedbed =Integer.parseInt(p[4]);
+                int year=Integer.parseInt(p[5]);
+                int month=Integer.parseInt(p[6]);
+                Hotel h= new Hotel(id,room,usedrooms,beds,usedbed,year,month );
+                hotels.add(h);
         }
+        for (Hotel h : hotels){
+            System.out.println(h);
+        }
+
+
+
+
+
+
+
+
+
     }
 }
--- src/main/java/org/example/pfusch1.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class pfusch1 {
-
-    public static void main(String[] args) {
-
-        System.out.printf("Hello World");
-    }
-}
